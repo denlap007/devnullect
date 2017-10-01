b08bf58cf2eb4fc0fecaa75279bcec7a464f6a0b
Dionysis Lappas <dio@freelabs.net>
2017-10-01 14:23:57 +0300

    Updated sql script to create tables for MySQl

--- create-tables.sql
+++ create-tables.sql
@@ -1,25 +1,25 @@
-CREATE TABLE group
+CREATE TABLE `group`
 (
-  group_id INT NOT NULL,
+  group_id INT NOT NULL AUTO_INCREMENT,
   group_name VARCHAR(32),
-  PRIMARY KEY (group_id)
-);
+  PRIMARY KEY ( group_id )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE user
+CREATE TABLE `user`
 (
-  user_id INT NOT NULL,
+  user_id INT NOT NULL AUTO_INCREMENT,
   username VARCHAR(32),
   group_id INT NOT NULL,
-  PRIMARY KEY (user_id),
-  FOREIGN KEY (group_id) REFERENCES group(group_id)
+  PRIMARY KEY ( user_id ),
+  FOREIGN KEY (group_id) REFERENCES `group`(group_id)
 );
 
 CREATE TABLE resource
 (
-  resource_id INT NOT NULL,
+  resource_id INT NOT NULL AUTO_INCREMENT,
   timestamp DATE NOT NULL,
   resource VARCHAR(500) NOT NULL,
-  PRIMARY KEY (resource_id)
+  PRIMARY KEY ( resource_id )
 );
 
 CREATE TABLE handles
