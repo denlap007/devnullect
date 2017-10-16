1648eedcaaaff6bf9d426ef043d1b08450119e3e
Dionysis Lappas <dio@freelabs.net>
2017-10-16 23:15:53 +0300

    Added new ER, Relational Schema, sql script to create tables

--- create-tables.sql
+++ create-tables.sql
@@ -1,33 +1,46 @@
 CREATE TABLE `group`
 (
   group_id INT NOT NULL AUTO_INCREMENT,
-  group_name VARCHAR(32),
   PRIMARY KEY ( group_id )
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `user`
 (
   user_id INT NOT NULL AUTO_INCREMENT,
-  username VARCHAR(32),
-  group_id INT NOT NULL,
-  PRIMARY KEY ( user_id ),
-  FOREIGN KEY (group_id) REFERENCES `group`(group_id)
+  PRIMARY KEY ( user_id )
 );
 
 CREATE TABLE resource
 (
   resource_id INT NOT NULL AUTO_INCREMENT,
-  timestamp DATE NOT NULL,
-  resource VARCHAR(500) NOT NULL,
-  PRIMARY KEY ( resource_id )
+  resource_content INT NOT NULL,
+  PRIMARY KEY (resource_id)
 );
 
-CREATE TABLE handles
+CREATE TABLE toDoList
 (
-  isHandled TINYINT(1) NOT NULL,
+  toDoList_id INT NOT NULL AUTO_INCREMENT,
+  toDoListName INT NOT NULL,
   user_id INT NOT NULL,
+  PRIMARY KEY (toDoList_id),
+  FOREIGN KEY (user_id) REFERENCES user(user_id)
+);
+
+
+CREATE TABLE group_user
+(
+  group_id INT NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (group_id, user_id),
+  FOREIGN KEY (group_id) REFERENCES `group`(group_id),
+  FOREIGN KEY (user_id) REFERENCES `user`(user_id)
+);
+
+CREATE TABLE resource_toDoList
+(
   resource_id INT NOT NULL,
-  PRIMARY KEY (user_id, resource_id),
-  FOREIGN KEY (user_id) REFERENCES user(user_id),
-  FOREIGN KEY (resource_id) REFERENCES resource(resource_id)
+  toDoList_id INT NOT NULL,
+  PRIMARY KEY (resource_id, toDoList_id),
+  FOREIGN KEY (resource_id) REFERENCES resource(resource_id),
+  FOREIGN KEY (toDoList_id) REFERENCES toDoList(toDoList_id)
 );
