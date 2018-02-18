a45d71b50b12149b2ad0e4f8e71ef9e86c0f78fc
Dionysis Lappas <dio@freelabs.net>
2018-02-18 17:04:21 +0200

    Fixed conf.ini

--- bootscript.sh
+++ bootscript.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Running bootscipt..." &&
 echo "Substituting environment variables..." &&
-envsubst < ${SRC_DIR}/config.ini > ${SRC_DIR}/config.ini &&
+envsubst < ${SRC_DIR}/conf.ini > ${SRC_DIR}/conf.ini &&
 echo "Starting bot service..." &&
 python ${SRC_DIR}/bot.py
